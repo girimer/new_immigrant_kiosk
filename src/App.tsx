5c8ff71d67747f27f9a85ba067cbae09d3e88e94
1r1di0us <57327873+1r1di0us@users.noreply.github.com>
2023-12-01 09:54:51 -0700

    added new screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomeMessage from "./WelcomePage";
 import MedicalScreen from "./pages/MedicalScreen";
 import LanguageScreen from "./pages/LanguageSelection";
+import TransitScreen1 from "./pages/TransitScreenMain";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomeMessage />} />
         <Route path="LanguageScreen/" element={<LanguageScreen />} />
+        <Route path="TransitScreen1/" element={<TransitScreen1 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/TransitScreenMain.tsx
@@ -0,0 +1,15 @@
+import LanguageButton from "../components/buttons/LanguageButton";
+import Typography from "@mui/material/Typography";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Button } from "@mui/material";
+export default function TransitScreen1() {
+    const iconStyle = {
+        padding: "16px", // Adjust the padding value as needed
+        fontSize: "7rem",
+    };
+    return(
+        <div>
+            
+        </div>
+    );
+}
