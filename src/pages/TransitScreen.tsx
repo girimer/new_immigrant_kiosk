52c24a1effe111583d91676603a6e14ed927f5b6
1r1di0us <57327873+1r1di0us@users.noreply.github.com>
2023-12-02 16:05:19 -0700

    finished transit1

--- src/pages/TransitScreen.tsx
+++ src/pages/TransitScreen.tsx
@@ -1,37 +1,46 @@
 import React from "react";
 import Header from "../components/Header";
-import "../components/buttons/ServicesButtton.css";
+import "../pages/TransitButtton.css";
 import Button from "../components/buttons/ServicesButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBus,
+  faDollarSign,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
+//import {
+//  faUser,
+//} from "@fortawesome/free-regular-svg-icons"; these icons give me errors
 
 const TransitPage: React.FC = () => {
   return (
     <>
       <Header />
       <div>
-        <h2 className="service-page-title">
+        <h2 className="transit-page-title">
           Commute and travel safely
         </h2>
-        <p className="service-page-description">
+        <p className="transit-page-description">
           No matter where you’re headed, Calgary Transit aims to keep you
           connected with people and places you care about by providing
           safe, accessible, reliable and courteous public transportation
           services. Our convenient transit service will get you to
           where you want to go.
         </p>
-        <div className="button-row">
+        <p className="transit-page-bold">
+          Tap to learn more about any of the following
+        </p>
+        <div className="transit-image"></div>
+        <div className="transit-buttons">
           <Button
             to="./Fares"
-            icon={<FontAwesomeIcon icon={faBus} />}
+            icon={<FontAwesomeIcon icon={faDollarSign} />}
           >
             Fares and Passes
           </Button>
           <Button
             to="./RiderInformation"
-            icon={<FontAwesomeIcon icon={faBus} />}
+            icon={<FontAwesomeIcon icon={faUser} />}
           >
             Rider Information
           </Button>
